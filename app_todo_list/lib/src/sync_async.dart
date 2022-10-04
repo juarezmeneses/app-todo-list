170919df75278784ca6e68bf6dd3098abfe9e2f2
juarezmeneses <juarezlmfilho@gmail.com>
2022-10-04 20:48:28 -0300

    Consuming external API (http)

--- app_todo_list/lib/src/sync_async.dart
+++ app_todo_list/lib/src/sync_async.dart
@@ -1,8 +1,22 @@
-void main(List<String> args) {
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Future main() async {
+  final json = await fetch();
+  print(json['id']);
   print4();
   print('1');
   print('2');
   print('3');
+}
+
+Future<Map> fetch() async {
+  var url = Uri.http('jsonplaceholder.typicode.com', '/todos/1');
+  var response = await http.get(url);
+  //print(response.body);
+  var json = jsonDecode(response.body);
+  return json;
 }
 
 Future<void> print4() async {

5922f01f3cae7c2ded6a80ef0ff62482837467ca
juarezmeneses <juarezlmfilho@gmail.com>
2022-10-06 22:40:11 -0300

    Object mapping (fromJson and toJson)

--- app_todo_list/lib/src/sync_async.dart
+++ app_todo_list/lib/src/sync_async.dart
@@ -3,23 +3,54 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future main() async {
-  final json = await fetch();
-  print(json['id']);
+  final todo = await fetch();
+  print(todo.toJson());
   print4();
   print('1');
   print('2');
   print('3');
 }
 
-Future<Map> fetch() async {
+Future<Todo> fetch() async {
   var url = Uri.http('jsonplaceholder.typicode.com', '/todos/1');
   var response = await http.get(url);
   //print(response.body);
   var json = jsonDecode(response.body);
-  return json;
+  var todo = Todo.fromJson(json);
+  return todo;
 }
 
 Future<void> print4() async {
   await Future.delayed(Duration(seconds: 2));
   print('4');
+}
+
+class Todo {
+  final String title;
+  final int id;
+  final int userId;
+  final bool completed;
+
+  Todo(
+      {required this.title,
+      required this.id,
+      required this.userId,
+      required this.completed});
+
+  factory Todo.fromJson(Map json) {
+    return Todo(
+        id: json['id'],
+        completed: json['completed'],
+        userId: json['userId'],
+        title: json['title']);
+  }
+
+  Map toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'completed': completed,
+      'userId': userId,
+    };
+  }
 }

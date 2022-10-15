ad56f51c8cc5e8c812539b41cb8ca95778fa66d0
juarezmeneses <juarezlmfilho@gmail.com>
2022-10-15 14:28:53 -0300

    Refactoring todo_repository.dart

--- app_todo_list/lib/src/repositories/todo_repository.dart
+++ app_todo_list/lib/src/repositories/todo_repository.dart
@@ -2,30 +2,16 @@ import 'package:app_todo_list/src/models/todo_model.dart';
 import 'package:dio/dio.dart';
 import 'package:mockito/annotations.dart';
 
-@GenerateMocks([TodoRepository])
 class TodoRepository {
   late Dio dio;
   final String url = 'https://jsonplaceholder.typicode.com/todos';
 
-  TodoRepository([Dio? client]) {
-    if (client == null) {
-      dio = Dio();
-    } else {
-      dio = client;
-    }
-  }
+  TodoRepository([Dio? client]) : dio = client ?? Dio();
 
   Future<List<TodoModel>> fetchTodos() async {
     final response = await dio.get(url);
     final list = response.data as List;
-
-    List<TodoModel> todos = [];
-    for (var json in list) {
-      final todo = TodoModel.fromJson(json);
-      todos.add(todo);
-    }
-
-    return todos;
+    return list.map((json) => TodoModel.fromJson(json)).toList();
   }
 }
 

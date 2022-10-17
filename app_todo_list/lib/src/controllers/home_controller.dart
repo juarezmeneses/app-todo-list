2abd16331c6ed9f6fa24b63b669ae4c9fbb50f4e
juarezmeneses <juarezlmfilho@gmail.com>
2022-10-16 22:17:55 -0300

    Added controller

---
+++ app_todo_list/lib/src/controllers/home_controller.dart
@@ -0,0 +1,12 @@
+import 'package:app_todo_list/src/models/todo_model.dart';
+import 'package:app_todo_list/src/repositories/todo_repository.dart';
+
+class HomeController {
+  List<TodoModel> todos = [];
+
+  final repository = TodoRepository();
+
+  Future start() async {
+    todos = await repository.fetchTodos();
+  }
+}

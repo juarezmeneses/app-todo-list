14743ae1423e221a88f1a9a9fb8ec0e0c345f3fc
juarezmeneses <juarezlmfilho@gmail.com>
2022-10-24 17:39:14 -0300

    View states

--- app_todo_list/lib/src/home_page.dart
+++ app_todo_list/lib/src/home_page.dart
@@ -1,7 +1,53 @@
+import 'package:app_todo_list/src/controllers/home_controller.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  _sucess() {
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text('item $index'),
+        );
+      },
+    );
+  }
+
+  _error() {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text('Retry'),
+      ),
+    );
+  }
+
+  _loading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  _start() {
+    return Container();
+  }
+
+  stateManagement(HomeState state) {
+    switch (state) {
+      case HomeState.start:
+        return _start();
+      case HomeState.loading:
+        return _loading();
+      case HomeState.error:
+        return _error();
+      case HomeState.sucess:
+        return _sucess();
+      default:
+        return _start();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,14 +55,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('List Todo\'s'),
       ),
-      body: ListView.builder(
-        itemCount: 20,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('item $index'),
-          );
-        },
-      ),
+      body: stateManagement(HomeState.start),
     );
   }
 }
